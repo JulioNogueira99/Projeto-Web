3ad6935208bb2216e7b1bb2a4d71b8c7e2638e95
Julio Nogueira <julio.nogueira561@gmail.com>
2021-08-17 15:32:47 -0300

    Delete seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -28,7 +28,7 @@ namespace SalesWebMVC.Controllers
         {
             var departments = _departmentService.FindAll();
             var viewModel = new SellerFormViewModel { Departments = departments };
-            return View(viewModel);
+            return View(viewModel   );
         }
         
         [HttpPost]
@@ -36,6 +36,30 @@ namespace SalesWebMVC.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
